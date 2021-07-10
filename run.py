f2b6437c928cab8993cac9bc989440f9a956d15f
thang42c3 <ducthangbnn@gmail.com>
2021-07-10 09:27:53 +0700

    Update

---
+++ run.py
@@ -0,0 +1,56 @@
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+from datetime import datetime
+from datetime import date
+import pymongo
+
+today = date.today()
+
+myclient = pymongo.MongoClient("mongodb+srv://ducthangbnn:Oivung1215@cluster0.1rpru.mongodb.net/test", connect=False)
+mydb = myclient["stocks"]
+mycol = mydb["price_9h_30_ex"]
+b30_stocks = mydb["b30_stock"]
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
+driver = webdriver.Chrome("chromedriver", chrome_options=options)
+
+
+for b30_stock in b30_stocks.find():
+    driver.get("https://www.cophieu68.vn/snapshot.php?id={0}".format(b30_stock["code"]))
+    time.sleep(3)
+    lists = []
+    a = driver.find_elements_by_xpath("//*[contains(text(), 'Xem chi tiết giao dịch trong ngày')]")[0]
+    a.click()
+    time.sleep(1)
+    html = driver.page_source
+    parse = BeautifulSoup(html, 'html.parser')
+    body = parse.find(id="trade_detail")
+    rows = body.find_all('tr')[1:]
+    lists = []
+    for row in rows:
+        columns = row.find_all('td')
+        list = []
+        for column in columns:
+            list.append(column.string)
+        lists.append(list)
+    print(lists)
+
+    min_value_list = []
+
+    for list in lists:
+        for i in range(0, len(lists)):
+            min_value_list.append(
+                abs(datetime.strptime(lists[i][0]+":00", '%H:%M:%S') - datetime.strptime('09:30:00', '%H:%M:%S')))
+
+    min_value = min(min_value_list)
+    index = min_value_list.index(min_value)
+    print(lists[index])
+
+    record = {"code": b30_stock["code"], "date": str(today), "time": lists[index][0]+":00","volume": lists[index][3],"price": lists[index][1], 'up_or_down': lists[index][2]}
+    mycol.insert_one(record)
+    time.sleep(3)
